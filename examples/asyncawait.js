dd001aa5ca20232f2590e520fc1024bb4ace1ed6
Hoang Phuong Le <lephuonghoang198@gmail.com>
2023-08-18 17:25:31 +0700

    feat(examples): implement example about aync await in JS

---
+++ examples/asyncawait.js
@@ -0,0 +1,12 @@
+const getData=()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>
+        resolve("some data"),2000);
+    })
+}
+
+const doSomething=async()=>{
+    const data= await getData()
+    console.log(data);
+}
+doSomething();
