4118588de0bf18764c050f142be41bbf128b0101
Hoang Phuong Le <lephuonghoang198@gmail.com>
2023-08-21 17:37:12 +0700

    feat(examples): add promise for comparison

--- examples/asyncawait.js
+++ examples/asyncawait.js
@@ -5,8 +5,13 @@ const getData=()=>{
     })
 }
 
+//use Promise
+getData().then(data1=>{
+    console.log("Use Promise:",data1);
+})
+
 const doSomething=async()=>{
     const data= await getData()
-    console.log(data);
+    console.log("Use Async, Await:",data);
 }
 doSomething();
