8b9148a8c96705375ae9e93bf78e09b97536bf5a
Hoang Phuong Le <lephuonghoang198@gmail.com>
2023-08-21 17:33:50 +0700

    feat(examples): add more examples about promises in JS

--- examples/promises.js
+++ examples/promises.js
@@ -1,6 +1,6 @@
 const doSomething= new Promise(
     (resolve, reject)=>{
-        let x=5;
+        let x=0;
         if (x===0){
             resolve("ok");
         } 
@@ -19,3 +19,45 @@ doSomething.then(
     }
 )
     
+const promiseExp=()=>{
+    return new Promise((resolve,reject)=>{
+        // resolve("get some data")
+        reject("something wrongs");
+    });
+}
+
+promiseExp().then(data=>{
+    console.log(data);
+})
+.catch(error=>{
+    console.log(error);
+})
+
+const promiseExp1=()=>{
+    return new Promise((resolve,reject)=>{
+        resolve({
+            name: "Le Hoang",
+            company: "AgilityIO"
+        })
+    });
+}
+
+promiseExp1().then(data1=>{
+    console.log(data1);
+})
+
+const promiseExp2=()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("promise exp 2");
+        },1000)
+    });
+}
+
+console.log(1);
+
+promiseExp2().then(data2=>{
+    console.log(data2);
+})
+
+console.log(2);
