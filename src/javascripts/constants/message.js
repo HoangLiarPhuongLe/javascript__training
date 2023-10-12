e17f8863425ee03d0fe6f892e4d44f9645f182bc
Hoang Phuong Le <lephuonghoang198@gmail.com>
2023-10-12 21:43:37 +0700

    feat(add transaction): implement add transaction UI

--- src/javascripts/constants/message.js
+++ src/javascripts/constants/message.js
@@ -2,9 +2,17 @@ export const MESSAGE = {
     EMAIL_REQUIRED: "The email is required",
     INVALID_EMAIL: "The email is invalid",
     PASSWORD_REQUIRED: "The password is required",
-    INVALID_PASSWORD: "The password is invalid"
+    INVALID_PASSWORD: "The password is invalid",
+    DATE_REQUIRED: "Date is required",
+    CATEGORY_REQUIRED: "Category is required",
+    AMOUNT_REQUIRED: "Amount is required",
+    NOTE_REQUIRED: "Note is required"
 }
 
 export const ERROR_MESSAGE = {
     ERROR_CREDENTIALS: "Email or Password not correct. Please try again !"
+}
+
+export const ADD_TRANSACTION_MESSAGE = {
+    ADD_TRANSACTION_SUCCES: "Add Transaction Succcess !"
 }
